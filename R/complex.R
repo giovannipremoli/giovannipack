de3c690def169d0439e74074139f9e5fcc20cac5
giovannipremoli <gio32.premoli@gmail.com>
2024-12-18 15:01:47 +0100

    Update complex.R

--- R/complex.R
+++ R/complex.R
@@ -5,7 +5,7 @@ complex <- function(x){
   else if(x<0){
     print("This is a negative number")
    }
-  else if(x==0){
+  else if(x==0){   #if we wrote just "else" we don't need to write the parenthesis
     print("This is just zero! No positive or negative!")
    }
   }
