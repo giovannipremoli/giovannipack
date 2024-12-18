b2ff101643f7cd33b573b65e5f3578b557ec3a5d
giovannipremoli <gio32.premoli@gmail.com>
2024-12-18 14:46:29 +0100

    Create complex.R

---
+++ R/complex.R
@@ -0,0 +1,11 @@
+complex <- function(x){
+  if(x>0){
+  print("This is a positive number")
+   }
+  else if(x<0){
+    print("This is a negative number")
+   }
+  else if(x==0){
+    print("This is just zero! No positive or negative!")
+   }
+  }
